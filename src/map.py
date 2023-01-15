10e047285ee96920bd3c8f84beb28ad752773a29
Rhys Davies <rhys.davies901@gmail.com>
2023-01-15 09:14:55 -0800

    Adding the start for rooms and requirements

--- src/map.py
+++ src/map.py
@@ -8,8 +8,7 @@ class Map:
     def __init__(self, width, height) -> None:
         self.width = width
         self.height = height
-        self.tiles = np.full((width, height), fill_value=tiles.floor, order="F")
-        self.tiles[30:33, 22] = tiles.wall
+        self.tiles = np.full((width, height), fill_value=tiles.wall, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
